3baafbf91e08f24d75fc9673bd7af87220211d9a
boomie101 <nathanielnjenga7@gmail.com>
2023-02-16 10:23:41 +0300

    lesson on numbers

---
+++ week-2/data_types.py
@@ -0,0 +1,21 @@
+#data types are used to store info
+#floating vs non-floating
+weight = 60.4 #floating
+english_grade = 33 #non floating
+x = 20
+y = 22
+z = 16
+x,y,z = 20,22,16 #this is a shorter way
+#characters - a,b,c,d.......
+bio_grade = "E" or 'Ç'
+#arithmetic progression
+z = x + y #addition
+p = x - y #subtraction
+q = x / y #quotient/divide
+r = x * y #product
+modulus = x % y #remainder
+square_x = x **2 #square of x   **means raise to power(then the number you want to square or cube)
+cube_x = x **3 #cube of x
+number = 13
+print("square of the number :",number **2)
+print("cube of the number :",number **3)
